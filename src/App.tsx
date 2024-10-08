e6428e37f4f0e5ad9d4ab44783eabec64792e8e2
rayjay-8 <rayarruda9876@gmail.com>
2024-10-08 19:34:13 -0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,57 @@
 import CountBtn from '@/components/CountBtn';
 import ReactSVG from '@/assets/react.svg';
 import { Badge } from '@/components/ui/badge';
+import { dados } from './contants';
+import { useLocalStorage } from 'usehooks-ts'
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Button } from './components/ui/button';
+
 
 function App() {
+
+  const [value, setValue, removeValue] = useLocalStorage('test-key', 0)
+
+  const usuariologado = dados.users.find(e => e.id === value)
+
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col items-center gap-y-4">
-        <div className="inline-flex items-center gap-x-4">
-          <img src={ReactSVG} alt="React Logo" className="w-32" />
-          <span className="text-6xl">+</span>
-          <img src={'/vite.svg'} alt="Vite Logo" className="w-32" />
-        </div>
-        <a href="https://ui.shadcn.com" rel="noopener noreferrer nofollow" target="_blank">
-          <Badge variant="outline">shadcn/ui</Badge>
-        </a>
-        <CountBtn />
+    <main className="">
+      <h1 className='text-4xl'>Ray + pix + p2p</h1>
+
+      <div className='flex gap-6'>
+      <p>Usuario: {usuariologado?.name}</p>
+      <p>Saldo: R${usuariologado?.totalcoin}</p>
       </div>
+
+      <div className='flex gap-6 items-center justify-center flex-wrap text-justify w-fill  '>
+
+      
+
+<Select onValueChange={(e) => setValue(e)}>
+  <SelectTrigger className="w-[180px]" >
+    <SelectValue placeholder={usuariologado?.name ?? "Logar"}  />
+  </SelectTrigger>
+  <SelectContent >
+
+  {dados.users.map(e => {
+        return(<SelectItem   key={e.id} value={e.id}>{e.name}</SelectItem>)
+      })}
+
+  </SelectContent>
+</Select>
+
+<Button onClick={() => {removeValue()}}>Logout</Button>
+
+</div>
+
+
+
     </main>
   );
 }
---
+++ src/contants.ts
@@ -0,0 +1,11 @@
+export const dados = {
+   users: [
+      {id: 1, email: "admin@gmail.com", totalcoin: 1, name: "admin", chavepix: "44920005175"},
+      {id: 2, email: "rayarruda9876@gmail.com", totalcoin: 0, name: "ray silva de arruda", chavepix: "44920005175"},
+   ]
+}
+
+
+export const historicotransacoes = [
+   {id: 1, userid: 1, operacao: "adicao", valor: 1, data: '08/10/2024 19:30:00', idbanco: 123}
+]
